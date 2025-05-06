7266368c424b07d32e01b060fc1669591224d7be
ikblmhndra <ikbalmahendra.96@gmail.com>
2025-05-06 15:07:19 +0700

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/once-ui/styles/index.scss";
 import "@/once-ui/tokens/index.scss";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 import classNames from "classnames";
 
@@ -130,6 +131,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
             </Flex>
             <Footer />
             <SpeedInsights/>
+            <Analytics/>
           </Column>
         </ToastProvider>
       </ThemeProvider>
